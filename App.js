da5fa09cef438cdfb537ae0c5613c8096765298f
Jay Jin <jayjinjay@gmail.com>
2018-08-18 22:05:19 +0800

    Add Camera

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Swiper from 'react-native-swiper'
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 var styles = StyleSheet.create({
   container: {
@@ -17,53 +17,94 @@ var styles = StyleSheet.create({
 class TitleText extends React.Component {
   render() {
     return (
-      <Text style={{ fontSize: 48, color: 'white' }}>
+      <Text style={{ fontSize: 48, color: 'black' }}>
         {this.props.label}
       </Text>
     )
   }
 }
 
-class App extends React.Component {
+class StamperCamera extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
 
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+  
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'black' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
+class App extends React.Component {
   viewStyle() {
     return {
       flex: 1,
-      backgroundColor: "red",
+      backgroundColor: 'transparent',
       justifyContent: 'center',
       alignItems: 'center',
+    }
+  }
+  cameraStyle() {
+    return {
+      flex: 1
     }
   }
 
   render() {
     return (
       <Swiper
+        horizontal={false}
         loop={false}
         showsPagination={false}
-        index={1}>
+        index={0}>
         <View style={this.viewStyle()}>
-          <TitleText label="Left" />
+          <TitleText label="Videos" />
         </View>
-        <Swiper
-          horizontal={false}
-          loop={false}
-          showsPagination={false}
-          index={1}>
-          <View style={this.viewStyle()}>
-            <TitleText label="Top" />
-          </View>
-          <View style={this.viewStyle()}>
-            <TitleText label="Home" />
-          </View>
-          <View style={this.viewStyle()}>
-            <TitleText label="Bottom" />
-          </View>
-        </Swiper>        
-        <View style={this.viewStyle()}>
-          <TitleText label="Right" />
+        <View style={this.cameraStyle()}>
+          <StamperCamera />
         </View>
-      </Swiper>
-      
+      </Swiper> 
     )
   }
 }
