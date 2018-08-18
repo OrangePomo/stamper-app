c7a5eafdbec70c258be949c79bb7c5b7a3a321da
Jay Jin <jayjinjay@gmail.com>
2018-08-18 21:05:27 +0800

    (WIP) Add react-native-camera

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import React from 'react'
+
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
 import Swiper from 'react-native-swiper'
-import { StyleSheet, Text, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
   },
   view: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
   }
 })
 
@@ -38,34 +63,49 @@ class App extends React.Component {
   render() {
     return (
       <Swiper
+        horizontal={false}
         loop={false}
         showsPagination={false}
         index={1}>
         <View style={this.viewStyle()}>
-          <TitleText label="Left" />
+          <TitleText label="Top" />
         </View>
-        <Swiper
-          horizontal={false}
-          loop={false}
-          showsPagination={false}
-          index={1}>
-          <View style={this.viewStyle()}>
-            <TitleText label="Top" />
-          </View>
-          <View style={this.viewStyle()}>
-            <TitleText label="Home" />
-          </View>
-          <View style={this.viewStyle()}>
-            <TitleText label="Bottom" />
-          </View>
-        </Swiper>        
         <View style={this.viewStyle()}>
-          <TitleText label="Right" />
+          <TitleText label="Home" />
         </View>
-      </Swiper>
-      
+        <View style={this.viewStyle()}>
+          <TitleText label="Camera(bottom)" />
+          <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style = {styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+          />
+          <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+          <TouchableOpacity
+              onPress={this.takePicture.bind(this)}
+              style = {styles.capture}
+          >
+              <Text style={{fontSize: 14}}> SNAP </Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+      </Swiper>        
     )
   }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      console.log(data.uri);
+    }
+  };
 }
 
 export default App
+AppRegistry.registerComponent('App', () => App);
